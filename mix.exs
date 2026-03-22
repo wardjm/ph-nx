df2c118a75832d376fdb4f6f662e691ff44d1913
Jeffrey Ward <wardjm@gmail.com>
2026-03-22 17:48:33 -0400

    Add package metadata and ex_doc dependency (closes #30)
    Added to mix.exs:
    - description, source_url, homepage_url
    - docs: [main: "readme", extras: ["README.md"]]
    - package: licenses and GitHub link
    - {:ex_doc, "~> 0.34", only: :dev, runtime: false}

--- mix.exs
+++ mix.exs
@@ -6,6 +6,17 @@ defmodule PhNx.MixProject do
       app: :ph_nx,
       version: "0.1.0",
       elixir: "~> 1.18",
+      description: "Persistent homology computation for Elixir using Nx",
+      source_url: "https://github.com/wardjm/ph-nx",
+      homepage_url: "https://github.com/wardjm/ph-nx",
+      docs: [
+        main: "readme",
+        extras: ["README.md"]
+      ],
+      package: [
+        licenses: ["MIT"],
+        links: %{"GitHub" => "https://github.com/wardjm/ph-nx"}
+      ],
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
@@ -22,7 +33,8 @@ defmodule PhNx.MixProject do
   defp deps do
     [
       {:nx, "~> 0.9"},
-      {:exla, "~> 0.11.0", runtime: Mix.env() != :test}
+      {:exla, "~> 0.11.0", runtime: Mix.env() != :test},
+      {:ex_doc, "~> 0.34", only: :dev, runtime: false}
     ]
   end
 end
